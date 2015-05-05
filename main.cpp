fe48339fe6ef05926a95667820a06d1874ded748
gehernandez1 <gehernandez1@broncs.utpa.edu>
2015-05-05 00:36:56 -0500

    phase3

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <stdlib.h>
+#include "command_line.h"
+
+using namespace std;
+
+int main()
+{
+
+    //declare shell class
+    shell instance;
+
+    //initialize shell
+    instance.runShell();
+
+    return 0;
+}
